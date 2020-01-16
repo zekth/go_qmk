08c8a9ac7b2db5fb730c105ad1818ba3bd1971f8
LE GOFF Vincent <g_n_s@hotmail.fr>
2020-01-16 23:09:36 +0100

    Adding worker pool

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/zekth/go_qmk
 
 go 1.13
+
+require github.com/gammazero/workerpool v0.0.0-20200108033143-79b2336fad7a
--- src/main.go
+++ src/main.go
@@ -2,10 +2,17 @@ package main
 
 import (
 	"fmt"
+
+	"github.com/gammazero/workerpool"
 )
 
 var Version string
 
 func main() {
 	fmt.Println("Start")
+	wp := workerpool.New(2)
+	wp.Submit(func() {
+		fmt.Println("Pooling")
+	})
+	wp.StopWait()
 }
