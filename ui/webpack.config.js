385554beab0d05d171fa403e085f00d1b04aa164
zekth <vince.legoff@gmail.com>
2020-01-17 21:23:07 +0100

    adding ui

---
+++ ui/webpack.config.js
@@ -0,0 +1,116 @@
+const path = require('path');
+const webpack = require('webpack');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const VueLoaderPlugin = require('vue-loader/lib/plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+
+function resolve(dir) {
+  return path.join(__dirname, dir);
+}
+
+module.exports = {
+  devServer: {
+    contentBase: path.join(__dirname, 'dist'),
+    compress: true,
+    port: 8081,
+    host: '127.0.0.1',
+    watchOptions: {
+      poll: true
+    },
+    open: true
+  },
+  entry: {
+    app: ['./src/main.ts']
+  },
+  output: {
+    filename: '[name].[hash].js',
+    path: path.resolve(__dirname, 'dist')
+  },
+  resolve: {
+    extensions: ['.ts', '.js', '.vue', '.json'],
+    alias: {
+      vue$: 'vue/dist/vue.esm.js',
+      '@': resolve('src')
+    },
+    modules: [path.resolve(__dirname, 'src'), 'node_modules']
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(ts|vue)$/,
+        loader: 'eslint-loader',
+        enforce: 'pre',
+        include: [resolve('src'), resolve('test')],
+        options: {
+          formatter: require('eslint-friendly-formatter'),
+          emitWarning: true
+        }
+      },
+      {
+        test: /\.vue$/,
+        loader: 'vue-loader',
+        options: {
+          loaders: {
+            css: ['vue-style-loader', 'css-loader', 'sass-loader'],
+            sass: ['vue-style-loader', 'css-loader', 'sass-loader'],
+            scss: ['vue-style-loader', 'css-loader', 'sass-loader']
+          }
+        }
+      },
+      {
+        test: /\.tsx?$/,
+        exclude: /node_modules/,
+        loader: 'ts-loader',
+        options: {
+          appendTsSuffixTo: [/\.vue$/]
+        }
+      },
+      {
+        test: /\.css$/,
+        use: ['vue-style-loader', 'css-loader']
+      },
+      {
+        test: /\.(sass|scss)$/,
+        use: [
+          {
+            loader: MiniCssExtractPlugin.loader
+          },
+          {
+            loader: 'css-loader'
+          },
+          {
+            loader: 'sass-loader'
+          }
+        ]
+      },
+      {
+        test: /\.(png|svg|jpg|ico|gif)$/,
+        use: ['file-loader']
+      }
+    ]
+  },
+  optimization: {
+    splitChunks: {
+      chunks: 'all'
+    }
+  },
+  plugins: [
+    new VueLoaderPlugin(),
+    new CleanWebpackPlugin(),
+    new webpack.DefinePlugin({
+      APP_REV: JSON.stringify(process.env.GITHUB_SHA || 'dev')
+    }),
+    new webpack.NamedModulesPlugin(),
+    new webpack.HotModuleReplacementPlugin(),
+    new HtmlWebpackPlugin({
+      filename: 'index.html',
+      template: 'src/index.html',
+      inject: true
+    }),
+    new MiniCssExtractPlugin({
+      filename: 'assets/css/[name].[contenthash].css',
+      chunkFilename: 'assets/css/[id].[contenthash].css'
+    })
+  ]
+};
