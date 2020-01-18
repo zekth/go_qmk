346b5cc476ec690db10c57cb8c166bb80c8c0340
zekth <vince.legoff@gmail.com>
2020-01-18 18:08:43 +0100

    Add env handling and hotreload

--- api/go.mod
+++ api/go.mod
@@ -1,8 +1,14 @@
-module github.com/zekth/go_qmk
+module github.com/zekth/go_qmk/api
 
 go 1.13
 
 require (
+	github.com/cespare/reflex v0.2.0 // indirect
+	github.com/fsnotify/fsnotify v1.4.7 // indirect
 	github.com/gammazero/workerpool v0.0.0-20200108033143-79b2336fad7a
 	github.com/gin-gonic/gin v1.5.0
+	github.com/kballard/go-shellquote v0.0.0-20180428030007-95032a82bc51 // indirect
+	github.com/kelseyhightower/envconfig v1.4.0
+	github.com/kr/pty v1.1.8 // indirect
+	github.com/ogier/pflag v0.0.1 // indirect
 )
--- api/src/main.go
+++ api/src/main.go
@@ -2,32 +2,47 @@ package main
 
 import (
 	"fmt"
+	"github.com/gammazero/workerpool"
 	"github.com/gin-gonic/gin"
-  "github.com/gammazero/workerpool"
-  "github.com/zekth/go_qmk/src/controllers"
+	"github.com/kelseyhightower/envconfig"
+	"github.com/zekth/go_qmk/api/src/controllers"
 )
 
 var Version string
 
 func foo() {
-  fmt.Println("Bar")
+	fmt.Println("Bar")
+}
+
+func workerpoolInjector(w *workerpool.WorkerPool) gin.HandlerFunc {
+	return func(c *gin.Context) {
+		c.Set("worker", w)
+		c.Next()
+	}
+}
+
+type EnvVars struct {
+	WorkerNumber int `default:2`
 }
 
 func main() {
-	fmt.Println("Start:"+Version)
-	wp := workerpool.New(2)
-	wp.Submit(func() {
-		fmt.Println("Pooling")
-	})
-  wp.StopWait()
-  
+	fmt.Println("Start:" + Version)
+	var env EnvVars
+	if err := envconfig.Process("go_qmk", &env); err != nil {
+		fmt.Println("Unable to get env vars")
+	}
+
+	wp := workerpool.New(env.WorkerNumber)
+
 	r := gin.New()
 
 	r.Use(gin.Logger())
-  r.Use(gin.Recovery())
-  
+	r.Use(gin.Recovery())
+	r.Use(workerpoolInjector(wp))
 	r.GET("/ping", controllers.Ping)
 
 	r.Static("/ui", "./ui")
-	r.Run()
+	if err := r.Run(); err != nil {
+		fmt.Println("Fatal error")
+	}
 }
